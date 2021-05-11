b84f0cee6784c67900cfc228b8df055e04b7d460
shikhars371 <shikhars371@gmail.com>
2021-05-11 15:27:13 +0530

    .

---
+++ messages.js
@@ -0,0 +1,28 @@
+
+ò
+schema.proto"$
+NoisePayload
+nonce (Rnonce"B
+Open
+deviceId (RdeviceId
+
+capability (R
+capability"
+Verify
+type (Rtype"
+Prove
+secret (Rsecret"!
+ReleaseSeed
+seed (Rseed")
+WantKey
+
+identifier (R
+identifier"F
+
+ProvideKey
+
+identifier (R
+identifier
+diffKey (RdiffKey"#
+Close
+deviceId (RdeviceId
